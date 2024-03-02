afc0f0f7d24f77f403c6d9b28ece8110d0458adf
ChenPy00 <453225733@qq.com>
2024-03-02 20:32:57 +0800

    first commit

---
+++ Readme.md
@@ -0,0 +1,3 @@
+# HelloWorld
+
+As a record of the first code upload to GitHub.
---
+++ main.py
@@ -0,0 +1,7 @@
+def print_hi(name):
+    # 在下面的代码行中使用断点来调试脚本。
+    print(f'Hi, {name}')
+
+
+if __name__ == '__main__':
+    print_hi('Peiyi Chen')
